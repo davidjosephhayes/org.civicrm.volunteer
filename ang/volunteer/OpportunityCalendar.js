99850b8576a593110a414d52f422fe36ff11d65d
David Hayes <david@blackbrick.software>
2019-11-20 00:00:14 -0800

    disable previous button if it takes into past

--- ang/volunteer/OpportunityCalendar.js
+++ ang/volunteer/OpportunityCalendar.js
@@ -56,6 +56,26 @@
           }
           $scope.volSignup(calEvent.id);
         },
+        viewRender: function(currentView){
+          const minDate = moment();
+          // maxDate = moment().add(2,'weeks');
+          // Past
+          if (minDate >= currentView.start && minDate <= currentView.end) {
+            $(".fc-prev-button").prop('disabled', true); 
+            $(".fc-prev-button").addClass('fc-state-disabled'); 
+          } else {
+            $(".fc-prev-button").removeClass('fc-state-disabled'); 
+            $(".fc-prev-button").prop('disabled', false); 
+          }
+          // // Future
+          // if (maxDate >= currentView.start && maxDate <= currentView.end) {
+          //   $(".fc-next-button").prop('disabled', true); 
+          //   $(".fc-next-button").addClass('fc-state-disabled'); 
+          // } else {
+          //   $(".fc-next-button").removeClass('fc-state-disabled'); 
+          //   $(".fc-next-button").prop('disabled', false); 
+          // }
+        }
       }
     };
     $scope.calendarSources = [];
